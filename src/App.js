e307065ef761ece3f710d14e71c77eb2e617cbb2
Rahim5357 <abdullrahim5357@gmail.com>
2024-10-18 23:22:32 +0530

    Read me update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 
-import './App.css';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+import Layout from './components/Layout';
+import EventList from './components/EventList';
+import SearchBar from './components/SearchBar';
+import Filters from './components/Filters';
+import UserAuth from './components/UserAuth';
+import EventDataChart from './components/EventDataChart';
+import EventPopularityChart from './components/EventPopularityChart';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <Provider store={store}>
+      <Layout>
+        <UserAuth />
+        <div className="container mx-auto px-4">
+          <SearchBar />
+          <Filters />
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+            <EventDataChart />
+            <EventPopularityChart />
+          </div>
+          <EventList />
+        </div>
+      </Layout>
+    </Provider>
   );
 }
 
